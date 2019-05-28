4200f8bbaa9ab7c22c20c30be4b4dc6b17cd7f6f
lilu <lilugirl2005@126.com>
2019-05-29 01:26:05 +0800

    创建入口文件

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { AxiosRequestConfig } from './types/index';
+
+
+function axios(config: AxiosRequestConfig) {
+  // todo
+
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Method = 'get' | 'GET'
+  | 'delete' | 'DELETE'
+  | 'head' | 'HEAD'
+  | 'options' | 'OPTIONS'
+  | 'post' | 'POST'
+  | 'put' | 'PUT'
+  | 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
