03e6b926e8b1e18966b780fa1efa9a657b3e3a6d
lilu <lilugirl2005@126.com>
2019-06-05 03:42:11 +0800

    处理请求body

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -62,3 +62,19 @@ axios({
   }
 })
 
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 31])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types/index';
 import xhr from './xhr';
 import { buildURL } from './helpers/url';
+import { transformRequest } from './helpers/data';
 
 
 function axios(config: AxiosRequestConfig): void {
@@ -10,11 +11,16 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
